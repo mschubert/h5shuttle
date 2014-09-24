e0cc164f3a54a48084bb7f82f1d2dc84be2e8a78
mschubert <mschu.dev@gmail.com>
2014-09-24 20:13:20 +0100

    support data.frame loading/saving in R, loading in py

--- h5shuttle/R/do.r
+++ h5shuttle/R/do.r
@@ -2,6 +2,7 @@
 #a = data.frame(matrix(rnorm(10),5,2))
 #colnames(a) = LETTERS[1:ncol(a)]
 #rownames(a) = LETTERS[1:nrow(a)]
+#a = cbind(a, xx=c("a","b","a","b","a"))
 #c = c(1:10)
 #d = setNames(3, LETTERS[5])
 #nodes = list(a=a, b=list(c=c, d=d))
@@ -16,15 +17,16 @@ library(rhdf5)
             npath = .jp(path, names(node)[i])
             h5createGroup(file, npath)
 
-            if (is.data.frame(nval)) #TODO: we do not support data.frames yet
-                nval = as.matrix(nval)
-
-            if (is.list(nval)) {
+            if (is.list(nval) && !is.data.frame(nval)) {
                 for (j in seq_along(nval))
                     node2group(file, npath, nval[j])
             } else {
-                nval = as.array(nval)
                 h5write(nval, file, .jp(npath,"value"))
+
+                if (is.data.frame(nval))
+                    nval = as.matrix(nval)
+
+                nval = as.array(nval)
                 for (j in 1:length(dim(nval)))
                    if (!is.null(dimnames(nval)[[j]]))
                        h5write(dimnames(nval)[[j]], file, .jp(npath,paste0("names_",j)))
